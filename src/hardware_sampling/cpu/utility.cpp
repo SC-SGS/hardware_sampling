fcb6373e67dfd95a2dd6e384e8e3aaf07b268ee3
Marcel Breyer <marcel.breyer@ipvs.uni-stuttgart.de>
2024-07-22 16:32:20 +0200

    Use correct error checking macro.

--- src/hardware_sampling/cpu/utility.cpp
+++ src/hardware_sampling/cpu/utility.cpp
@@ -50,7 +50,7 @@ std::string run_subprocess(const std::string_view cmd_line) {
     const std::size_t bytes_read = std::fread(buffer.data(), sizeof(typename decltype(buffer)::value_type), buffer.size(), out_handle);
 
     // destroy subprocess
-    PLSSVM_SUBPROCESS_ERROR_CHECK(subprocess_join(&proc, &return_code));
+    HWS_SUBPROCESS_ERROR_CHECK(subprocess_join(&proc, &return_code));
 
     // create output
     return buffer.substr(0, bytes_read);
