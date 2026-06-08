04210fb64f642c252b9cdfc3501de22da17d0979
TimThuering <56958925+TimThuering@users.noreply.github.com>
2026-06-08 15:48:58 +0200

    python bindings for MPI-aware constructor and functions

--- bindings/system_hardware_sampler.cpp
+++ bindings/system_hardware_sampler.cpp
@@ -20,21 +20,70 @@
 #include <string>              // std::string
 
 #if defined(HWS_MPI_SUPPORT_ENABLED)
-#include <mpi.h>
-#include "mpi4py_communicator.hpp"
+    #include "mpi4py_communicator.hpp"
+    #include <mpi.h>
 #endif
 
 namespace py = pybind11;
 
 void init_system_hardware_sampler(py::module_ &m) {
+#if defined(HWS_MPI_SUPPORT_ENABLED)
+    // bind mpi sampling mode enum
+    py::enum_<hws::detail::mpi_sampling_mode>(m, "MPISamplingMode")
+        .value("PER_RANK", hws::detail::mpi_sampling_mode::per_rank)
+        .value("WHOLE_NODE", hws::detail::mpi_sampling_mode::whole_node)
+        .export_values();
+#endif
     // bind the pure virtual hardware sampler base class
     py::class_<hws::system_hardware_sampler>(m, "SystemHardwareSampler")
         .def(py::init<>(), "construct a new system hardware sampler with the default sampling interval")
         .def(py::init<hws::sample_category>(), "construct a new system hardware sampler with the default sampling interval sampling only the provided sample_category samples")
         .def(py::init<std::chrono::milliseconds>(), "construct a new system hardware sampler for with the specified sampling interval")
         .def(py::init<std::chrono::milliseconds, hws::sample_category>(), "construct a new system hardware sampler for with the specified sampling interval sampling only the provided sample_category samples")
+#if defined(HWS_MPI_SUPPORT_ENABLED)
+        // MPI-aware constructors
+
+        // (MPI_Comm, mode, category=all)
+        .def(py::init([](py::object py_comm,
+                         hws::detail::mpi_sampling_mode mode,
+                         hws::sample_category category) {
+                 MPI_Comm comm = mpi_comm_from_python(py_comm);
+                 return std::make_unique<hws::system_hardware_sampler>(comm, mode, category);
+             }),
+             py::arg("comm"),
+             py::arg("mode"),
+             py::arg("category") = hws::sample_category::all,
+             "construct a new system hardware sampler with the default sampling interval and MPI support using the given mpi4py communicator and sampling mode")
+
+        // (MPI_Comm, mode, sampling_interval, category=all)
+        .def(py::init([](py::object py_comm,
+                         hws::detail::mpi_sampling_mode mode,
+                         std::chrono::milliseconds sampling_interval,
+                         hws::sample_category category) {
+                 MPI_Comm comm = mpi_comm_from_python(py_comm);
+                 return std::make_unique<hws::system_hardware_sampler>(comm, mode, sampling_interval, category);
+             }),
+             py::arg("comm"),
+             py::arg("mode"),
+             py::arg("sampling_interval"),
+             py::arg("category") = hws::sample_category::all,
+             "construct a new system hardware sampler with the specified sampling interval and MPI support using the given mpi4py communicator and sampling mode")
+
+        // Non-MPI overloads
+        .def("start", py::overload_cast<>(&hws::system_hardware_sampler::start_sampling), "start hardware sampling for all available hardware samplers")
+        .def("stop", py::overload_cast<>(&hws::system_hardware_sampler::stop_sampling), "stop hardware sampling for all available hardware samplers")
+        // MPI-aware overloads
+        .def("start", [](hws::system_hardware_sampler &self, py::object py_comm) {
+                 MPI_Comm comm = mpi_comm_from_python(py_comm);
+                 self.start_sampling(comm); }, py::arg("comm"), "start hardware sampling for all available hardware samplers; executes an MPI barrier on the given communicator before starting")
+        .def("stop", [](hws::system_hardware_sampler &self, py::object py_comm) {
+                 MPI_Comm comm = mpi_comm_from_python(py_comm);
+                 self.stop_sampling(comm); }, py::arg("comm"), "stop hardware sampling for all available hardware samplers; executes an MPI barrier on the given communicator after stopping")
+#else
+        // No MPI support: only the simple overloads exist, no ambiguity
         .def("start", &hws::system_hardware_sampler::start_sampling, "start hardware sampling for all available hardware samplers")
-        .def("stop", &hws::system_hardware_sampler::stop_sampling, "stop hardware sampling for all available hardware samplers")
+        .def("stop", &hws::system_hardware_sampler::stop_sampling, "stop hardware sampling for all available hardware samplers");
+#endif
         .def("pause", &hws::system_hardware_sampler::pause_sampling, "pause hardware sampling for all available hardware samplers")
         .def("resume", &hws::system_hardware_sampler::resume_sampling, "resume hardware sampling for all available hardware samplers")
         .def("has_started", &hws::system_hardware_sampler::has_sampling_started, "check whether hardware sampling has already been started for all hardware samplers")
@@ -73,17 +122,9 @@ void init_system_hardware_sampler(py::module_ &m) {
         .def("dump_yaml", py::overload_cast<const std::string &>(&hws::system_hardware_sampler::dump_yaml, py::const_), "dump all hardware samples for all hardware samplers to the given YAML file")
         .def("as_yaml_string", &hws::system_hardware_sampler::as_yaml_string, "return all hardware samples for all hardware samplers as YAML string")
 #if defined(HWS_MPI_SUPPORT_ENABLED)
-        .def("dump_yaml_global",
-            [](const hws::system_hardware_sampler &self,
-               const std::string &filename,
-               py::object py_comm) {
+        .def("dump_yaml_global", [](const hws::system_hardware_sampler &self, const std::string &filename, py::object py_comm) {
                 const MPI_Comm comm = mpi_comm_from_python(py_comm);
-                self.dump_yaml_global(filename, comm);
-            },
-            py::arg("filename"),
-            py::arg("comm"),
-            "Let MPI rank 0 dump the hardware samples of all hardware samplers of all MPI ranks to the given YAML file using the provided mpi4py communicator."
-        )
+                self.dump_yaml_global(filename, comm); }, py::arg("filename"), py::arg("comm"), "Let MPI rank 0 dump the hardware samples of all hardware samplers of all MPI ranks to the given YAML file using the provided mpi4py communicator.")
 #endif
         .def("__repr__", [](const hws::system_hardware_sampler &self) { return fmt::format("<hws.SystemHardwareSampler with {} samples>", self.num_samplers()); });
 }
